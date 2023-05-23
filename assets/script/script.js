85237a16a3e1818353a68caf4364e196671472d4
Wilfried Muyisa <68196203+WilfriedM01@users.noreply.github.com>
2023-05-23 16:55:28 +0100

    Update UI style

--- assets/script/script.js
+++ assets/script/script.js
@@ -7,12 +7,12 @@ const ContactButton = document.getElementById('contact_buttton');
 
 function close() {
   MobileMenu.style.display = 'none';
-  document.body.style.overflow='auto';
+  document.body.style.overflow = 'auto';
 }
 
 function open() {
   MobileMenu.style.display = 'block';
-  document.body.style.overflow='hidden';
+  document.body.style.overflow = 'hidden';
 }
 
 CloseButton.addEventListener('click', close);
